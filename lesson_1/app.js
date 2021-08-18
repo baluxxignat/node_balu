67cb7dbcc4f9f3d6bc5d4583f96905ece76286a3
baluxxignat <aleksandr.pravo@gmail.com>
2021-08-18 14:29:26 +0300

    homework done

--- lesson_1/app.js
+++ lesson_1/app.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const fs = require('fs');
-
 // path to folders creating
 const pathTo1800 = path.join(__dirname, '1800');
 const pathTo2000 = path.join(__dirname, '2000');
@@ -8,13 +7,14 @@ const pathTo2000 = path.join(__dirname, '2000');
 
 
 
+                                        ////////       MALE   ///////////////////
+
 // trying to read folders content
 fs.readdir(pathTo1800, ((err, files) => {
     if (err) {
         console.log(err);
         return;
     }
-
     files.forEach(item => {
         const itemPath = path.join(pathTo1800, item);        // all items full path inside folder 1800
         const newPath = path.join(pathTo2000, item);
@@ -26,13 +26,8 @@ fs.readdir(pathTo1800, ((err, files) => {
                 console.log(err);
                 return;
             }
-
             if (stats.isFile()) {
                 fs.readFile(itemPath, ((err2, data) => {
-                    if (err) {
-                        console.log(err);
-                        return;
-                    }
                     let itemToJson = JSON.parse(data);
                     //console.log(itemToJson);                                    //checking what i got: keys and values
                     if (itemToJson.gender === 'male') {
@@ -40,8 +35,40 @@ fs.readdir(pathTo1800, ((err, files) => {
                             console.log(err3);
                         })
                     }
+                }))
+            }
+        }))
+    })
+}))
 
 
+                                                /////////////FEMALE//////////////////
+
+fs.readdir(pathTo2000, ((err, files) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    files.forEach(item2 => {
+        const itemPath2 = path.join(pathTo2000, item2);        // all items full path inside folder 1800
+        const newPath2 = path.join(pathTo1800, item2);
+        //console.log(itemPath);
+
+        // check tip of included files
+        fs.stat(itemPath2, ((err1, stats) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (stats.isFile()) {
+                fs.readFile(itemPath2, ((err2, data) => {
+                    let itemToJson = JSON.parse(data);
+                    //console.log(itemToJson);                                    //checking what i got: keys and values
+                    if (itemToJson.gender === 'female') {
+                        fs.rename(itemPath2, newPath2, err3 => {             //moving items
+                            console.log(err3);
+                        })
+                    }
                 }))
             }
         }))
