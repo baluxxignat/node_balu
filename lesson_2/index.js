51b9a71a6d3a67f3cbc3e7f61693662afefd704b
baluxxignat <aleksandr.pravo@gmail.com>
2021-08-19 14:31:42 +0300

    clear server

--- lesson_2/index.js
+++ lesson_2/index.js
@@ -1,1 +1,18 @@
-console.log('hello2');
+const  express = require('express');
+const expressHbs = require('express-handlebars');
+const  path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.set ('view engine', '.hbs');
+app.engine('.hbs', expressHbs({defaultLayout: false}));
+app.set ('view', path.join(__dirname, 'static'));
+
+app.listen(5000, () => {
+    console.log('App listen 5000');
+});
+
+app.get('/ping', ((req, res) => {
+    res.json ('Pong');
+}))
